4fef2f0eba03725f0ecfe836dfc7df00094f2cfe
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2024-01-16 11:47:17 -0600

    Add custom theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_clean/presentation/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
   // Private constructor
   MyApp._internal();
 
+  int appState = 0;
+
   // Single instance of MyApp - Singleton
   static final MyApp instance = MyApp._internal();
+
+  // Factory for the class instance
+  factory MyApp() => instance;
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -16,6 +20,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+    );
   }
 }
