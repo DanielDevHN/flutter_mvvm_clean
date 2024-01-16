aaafee983e8da29b88958003d71227774b350214
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2024-01-16 11:45:49 -0600

    Add font manager

---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = 'Monsterrat';
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static const double small = 12;
+  static const double medium = 14;
+  static const double large = 16;
+  static const double xLarge = 18;
+  static const double xxLarge = 20;
+}
